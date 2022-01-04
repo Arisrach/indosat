8ae8f6931d6b4de5bde8f10c0f12f049d4215066
Arisrach <58836075+Arisrach@users.noreply.github.com>
2022-01-04 11:54:33 +0700

    drop area, select

--- assets/js/droparea.js
+++ assets/js/droparea.js
@@ -632,16 +632,17 @@
   };
 
   // Attach the plugin to jQuery
-  // $.fn.droparea = function(method) {
-  //   if (methods[method]) {
-  //     return methods[method].apply(
-  //       this,
-  //       Array.prototype.slice.call(arguments, 1)
-  //     );
-  //   } else if (typeof method === 'object' || !method) {
-  //     return methods.init.apply(this, arguments);
-  //   } else {
-  //     $.error('Method ' + method + ' does not exist on Droparea.js');
-  //   }
-  // };
+  $.fn.droparea = function(method) {
+    if (methods[method]) {
+      return methods[method].apply(
+        this,
+        Array.prototype.slice.call(arguments, 1)
+      );
+    } 
+    else if (typeof method === 'object' || !method) {
+      return methods.init.apply(this, arguments);
+    } else {
+      $.error('Method ' + method + ' does not exist on Droparea.js');
+    }
+  };
 })(jQuery);
